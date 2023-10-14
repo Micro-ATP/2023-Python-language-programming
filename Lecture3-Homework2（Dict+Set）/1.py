bf4efc22b7c2fe677e5ef562d0b97a56b86b1ea9
陆岩淅 <131521928+Micro-ATP@users.noreply.github.com>
2023-10-14 12:14:43 +0800

    Update 1.py

--- Lecture3-Homework2（Dict+Set）/1.py
+++ Lecture3-Homework2（Dict+Set）/1.py
@@ -1,69 +1,69 @@
-class StudentManagementSystem:
-    def __init__(self):
-        self.studentGrades = {}
+# 创建一个空的学生管理系统
+studentGrades = {}
 
-    def addStudent(self, name, grade):
-        self.studentGrades[name] = grade
+# 添加学生和成绩
+def addStudent(name, grade):
+    studentGrades[name] = grade
+    print(f"学生 '{name}' 的成绩已添加为 {grade}")
 
-    def queryGrade(self, name):
-        if name in self.studentGrades:
-            return self.studentGrades[name]
-        else:
-            return "Student not found."
+# 查找学生成绩
+def findGrade(name):
+    if name in studentGrades:
+        print(f"学生 '{name}' 的成绩是 {studentGrades[name]}")
+    else:
+        print(f"找不到学生 '{name}' 的成绩")
 
-    def updateGrade(self, name, newGrade):
-        if name in self.studentGrades:
-            self.studentGrades[name] = newGrade
-            return "Grade updated successfully."
-        else:
-            return "Student not found."
+# 修改学生成绩
+def updateGrade(name, newGrade):
+    if name in studentGrades:
+        studentGrades[name] = newGrade
+        print(f"学生 '{name}' 的成绩已更新为 {newGrade}")
+    else:
+        print(f"找不到学生 '{name}' 的成绩")
 
-    def deleteStudent(self, name):
-        if name in self.studentGrades:
-            del self.studentGrades[name]
-            return "Student deleted successfully."
-        else:
-            return "Student not found."
+# 删除学生成绩
+def deleteStudent(name):
+    if name in studentGrades:
+        del studentGrades[name]
+        print(f"学生 '{name}' 的成绩已删除")
+    else:
+        print(f"找不到学生 '{name}' 的成绩")
 
-    def printAllStudents(self):
-        for name, grade in self.studentGrades.items():
-            print(f"{name}: {grade}")
+# 打印所有学生和成绩
+def printAllStudents():
+    for name, grade in studentGrades.items():
+        print(f"{name}: {grade}")
 
-def main():
-    sms = StudentManagementSystem()
-    while True:
-        print("\nStudent Management System")
-        print("1. Add Student")
-        print("2. Search Grade")
-        print("3. Update Grade")
-        print("4. Delete Student")
-        print("5. Print All Students")
-        print("6. Quit")
+# 主程序
+while True:
+    print("\n学生管理系统")
+    print("1. 添加学生成绩")
+    print("2. 查找学生成绩")
+    print("3. 修改学生成绩")
+    print("4. 删除学生成绩")
+    print("5. 打印所有学生成绩")
+    print("6. 退出")
 
-        choice = input("Enter your choice: ")
+    choice = input("请选择操作 (1/2/3/4/5/6): ")
 
-        if choice == "1":
-            name = input("Enter student's name: ")
-            grade = input("Enter student's grade: ")
-            sms.addStudent(name, grade)
-            print("Student added successfully.")
-        elif choice == "2":
-            name = input("Enter student's name: ")
-            grade = sms.queryGrade(name)
-            print(f"Grade for {name}: {grade}")
-        elif choice == "3":
-            name = input("Enter student's name: ")
-            newGrade = input("Enter new grade: ")
-            result = sms.updateGrade(name, newGrade)
-            print(result)
-        elif choice == "4":
-            name = input("Enter student's name: ")
-            result = sms.deleteStudent(name)
-            print(result)
-        elif choice == "5":
-            sms.printAllStudents()
-        elif choice == "6":
-            print(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>")
-            break
-        else:
-            print("ERROR. Please try again.")
+    if choice == '1':
+        name = input("请输入学生姓名: ")
+        grade = input("请输入学生成绩: ")
+        addStudent(name, grade)
+    elif choice == '2':
+        name = input("请输入学生姓名: ")
+        findGrade(name)
+    elif choice == '3':
+        name = input("请输入学生姓名: ")
+        newGrade = input("请输入新的成绩: ")
+        updateGrade(name, newGrade)
+    elif choice == '4':
+        name = input("请输入学生姓名: ")
+        deleteStudent(name)
+    elif choice == '5':
+        printAllStudents()
+    elif choice == '6':
+        print("退出系统。")
+        break
+    else:
+        print("无效的选项，请重新选择。")
