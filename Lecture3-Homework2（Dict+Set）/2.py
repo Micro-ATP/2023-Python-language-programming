00577135890c0d534c73d27d52de719abbe7fb95
陆岩淅 <131521928+Micro-ATP@users.noreply.github.com>
2023-10-14 12:08:37 +0800

    Update 2.py

--- Lecture3-Homework2（Dict+Set）/2.py
+++ Lecture3-Homework2（Dict+Set）/2.py
@@ -1,67 +1,60 @@
-class MovieRatingSystem:
-    def __init__(self):
-        self.movieRatings = {}
+# 创建一个空的电影评分字典
+movieRatings = {}
 
-    def addMovieRating(self, movieName, rating):
-        self.movieRatings[movieName] = rating
+# 添加电影和评分
+def addMovieRating(movieName, rating):
+    movieRatings[movieName] = rating
+    print(f"电影 '{movieName}' 的评分已添加为 {rating}")
 
-    def findMovieRating(self, movieName):
-        return self.movieRatings.get(movieName, "Movie not found.")
+# 查找电影评分
+def findMovieRating(movieName):
+    if movieName in movieRatings:
+        print(f"电影 '{movieName}' 的评分是 {movieRatings[movieName]}")
+    else:
+        print(f"找不到电影 '{movieName}' 的评分")
 
-    def modifyMovieRating(self, movieName, newRating):
-        if movieName in self.movieRatings:
-            self.movieRatings[movieName] = newRating
-            return "Movie rating updated successfully."
-        else:
-            return "Movie not found."
+# 修改电影评分
+def modifyMovieRating(movieName, newRating):
+    if movieName in movieRatings:
+        movieRatings[movieName] = newRating
+        print(f"电影 '{movieName}' 的评分已更新为 {newRating}")
+    else:
+        print(f"找不到电影 '{movieName}' 的评分")
 
-    def deleteMovieRating(self, movieName):
-        if movieName in self.movieRatings:
-            del self.movieRatings[movieName]
-            return "Movie rating deleted successfully."
-        else:
-            return "Movie not found."
+# 删除电影评分
+def deleteMovieRating(movieName):
+    if movieName in movieRatings:
+        del movieRatings[movieName]
+        print(f"电影 '{movieName}' 的评分已删除")
+    else:
+        print(f"找不到电影 '{movieName}' 的评分")
 
-    def printAllMoviesAndRatings(self):
-        for movie, rating in self.movieRatings.items():
-            print(f"{movie}: {rating}")
-
-def main():
-    movieSystem = MovieRatingSystem()
-
-    while True:
-        print("\nMovie Rating System")
-        print("1. Add Movie Rating")
-        print("2. Find Movie Rating")
-        print("3. Modify Movie Rating")
-        print("4. Delete Movie Rating")
-        print("5. Print All Movies and Ratings")
-        print("6. Quit")
-
-        choice = input("Enter your choice: ")
-
-        if choice == "1":
-            movieName = input("Enter movie name: ")
-            rating = input("Enter movie rating (0.0 to 10.0): ")
-            movieSystem.addMovieRating(movieName, float(rating))
-            print("Movie rating added successfully.")
-        elif choice == "2":
-            movieName = input("Enter movie name: ")
-            rating = movieSystem.findMovieRating(movieName)
-            print(f"Rating for {movieName}: {rating}")
-        elif choice == "3":
-            movieName = input("Enter movie name: ")
-            newRating = input("Enter new movie rating (0.0 to 10.0): ")
-            result = movieSystem.modifyMovieRating(movieName, float(newRating))
-            print(result)
-        elif choice == "4":
-            movieName = input("Enter movie name: ")
-            result = movieSystem.deleteMovieRating(movieName)
-            print(result)
-        elif choice == "5":
-            movieSystem.printAllMoviesAndRatings()
-        elif choice == "6":
-            print("Goodbye!")
-            break
-        else:
-            print("Invalid choice. Please try again.")
+# 主程序
+while True:
+    print("\n电影评分系统")
+    print("1. 添加电影评分")
+    print("2. 查找电影评分")
+    print("3. 修改电影评分")
+    print("4. 删除电影评分")
+    print("5. 退出")
+    
+    choice = input("请选择操作 (1/2/3/4/5): ")
+    
+    if choice == '1':
+        movieName = input("请输入电影名称: ")
+        rating = float(input("请输入评分: "))
+        addMovieRating(movieName, rating)
+    elif choice == '2':
+        movieName = input("请输入电影名称: ")
+        findMovieRating(movieName)
+    elif choice == '3':
+        movieName = input("请输入电影名称: ")
+        newRating = float(input("请输入新的评分: "))
+        modifyMovieRating(movieName, newRating)
+    elif choice == '4':
+        movieName = input("请输入电影名称: ")
+        deleteMovieRating(movieName)
+    elif choice == '5':
+        break
+    else:
+        print("无效的选项，请重新选择。")
