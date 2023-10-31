697edc0b052cb7f5ec245403998af38c3cac1cdb
陆岩淅 <131521928+Micro-ATP@users.noreply.github.com>
2023-10-31 16:19:44 +0800

    Create 4.py

---
+++ Homework/Lecture 5-Homework/4.py
@@ -0,0 +1,19 @@
+def quickSort(a):
+    if len(a) <= 1:
+        return a
+    else:
+        nvidia = a[0]  
+        lessThanNvidia = []  
+        greaterThanNvidia = []  
+        for i in a[1:]:  
+            if i <= nvidia:
+                lessThanNvidia.append(i)  
+            else:
+                greaterThanNvidia.append(i)  
+        
+        return quickSort(lessThanNvidia) + [nvidia] + quickSort(greaterThanNvidia)
+
+
+# a = input("请输入一组数字，以空格分隔：")
+# sortedArr = quickSort(a)
+# print("排序后的数组:", sortedArr)
