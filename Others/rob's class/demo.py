65357cb865a84bf61a74a78218547c707e1743ca
陆岩淅 <131521928+Micro-ATP@users.noreply.github.com>
2023-12-20 17:33:18 +0800

    Create demo.py

---
+++ Others/rob's class/demo.py
@@ -0,0 +1,34 @@
+from collections import defaultdict
+
+def group_anagrams(strs):
+    anagrams_dict = defaultdict(list)
+
+    for s in strs:
+        key = tuple(sorted(s))
+        anagrams_dict[key].append(s)
+
+    result = [set(group) for group in anagrams_dict.values()]
+
+    return result
+
+input_strs = ["listen", "cab", "abc", "silent", "hello", "hit", "enlist", "ith"]
+output_groups = group_anagrams(input_strs)
+print(output_groups)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# powered by Genshin Impact
